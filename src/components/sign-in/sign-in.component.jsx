67284514d749249ba1a87d2bb0f8e859c968e1da
Steven De Avila <stevejd88@gmail.com>
2021-01-07 14:46:07 -0600

    integrate firebase into application, create sign in page with sign in component and sign in with google

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
+import { signInWithGoogle } from '../../firebase/firebase.utils';
+
 import './sign-in.styles.scss';
 
 export class SignIn extends Component {
@@ -51,7 +53,12 @@ export class SignIn extends Component {
             label='password'
             required
           />
-          <CustomButton type='submit'>Sign In</CustomButton>
+          <div className='buttons'>
+            <CustomButton type='submit'>Sign In</CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+              Sign In with Google
+            </CustomButton>
+          </div>
         </form>
       </div>
     );
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+  apiKey: 'AIzaSyCDQOK3wI1RYXx0YcQAc_LCgAjumI4gyl4',
+  authDomain: 'king-crwndb.firebaseapp.com',
+  projectId: 'king-crwndb',
+  storageBucket: 'king-crwndb.appspot.com',
+  messagingSenderId: '919662457697',
+  appId: '1:919662457697:web:9e8d2243ee528277eccb20',
+  measurementId: 'G-FKTJ17YSVV'
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
