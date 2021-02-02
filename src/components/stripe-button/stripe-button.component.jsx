0602c522606dfa916ba726debdd51d54879b39b6
Steven De Avila <stevejd88@gmail.com>
2021-02-02 10:31:09 -0600

    add stripe button payment component

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    'pk_test_51IGR8oL3z315KuOMxiocT8B9n1ID7ScgiFFZcgMpA6ynxHgVNFlQsF01uua5806uYlwMvBRECHaCPuyJkeuxlM8700pe0floXR';
+
+  const onToken = (token) => {
+    console.log(token);
+    alert('Payment Successful');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing'
+      billingAddress
+      shippingAddress
+      image='https://sendeyo.com/up/d/f3eb2117da'
+      description={`Your total is $${price}`}
+      amouunt={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
