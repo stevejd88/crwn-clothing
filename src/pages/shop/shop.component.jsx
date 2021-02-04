e60317096ff290149eceaba4401fa6a2710d9cd5
Steven De Avila <stevejd88@gmail.com>
2021-02-04 10:48:36 -0600

    Add WithSpinner higher order component to CollectionOverview and CollectionsPage rendered in ShopPage component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -12,7 +12,16 @@ import {
 
 import { updateCollections } from '../../redux/shop/shop.actions';
 
+import WithSpinner from '../../components/with-spinner/with-spinner.component';
+
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
+
 class ShopPage extends Component {
+  state = {
+    loading: true
+  };
+
   unsubscripeFromSnapshot = null;
 
   componentDidMount() {
@@ -23,18 +32,28 @@ class ShopPage extends Component {
       async (snapshot) => {
         const collectionsMap = convertCollectionsSnapshotMap(snapshot);
         updateCollections(collectionsMap);
+        this.setState({ loading: false });
       }
     );
   }
 
   render() {
     const { match } = this.props;
+    const { loading } = this.state;
     return (
       <div className='shop-page'>
-        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route
+          exact
+          path={`${match.path}`}
+          render={(props) => (
+            <CollectionsOverviewWithSpinner isLoading={loading} {...props} />
+          )}
+        />
         <Route
           path={`${match.path}/:collectionID`}
-          component={CollectionPage}
+          render={(props) => (
+            <CollectionPageWithSpinner isLoading={loading} {...props} />
+          )}
         />
       </div>
     );
